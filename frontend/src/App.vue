90b86f7b9c26a82b433e0c31a5fce76b1508aa4e
Nishanth964 <nishanthnaik964@gmail.com>
2026-01-01 19:10:20 +0530

    added new components for darkmode and github button (only github button works)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
-
-
+    import GithubButton from './components/pages/home/homegithub.vue';
+    import darkmodeSwitch from './components/pages/home/darkmode.vue';
 </script>
 
 <template>
-
-
+    <div>
+        <GithubButton/>
+        <darkmodeSwitch/>
+    </div>
 </template>
 
 <style scoped>
