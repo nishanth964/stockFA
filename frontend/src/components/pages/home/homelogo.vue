67d3d4e160b50fcf5642a2b6d5177bd98df39cf4
Nishanth964 <nishanthnaik964@gmail.com>
2026-01-03 09:39:20 +0530

    gitignores added and modified

--- frontend/src/components/pages/home/homelogo.vue
+++ frontend/src/components/pages/home/homelogo.vue
@@ -1,30 +1,32 @@
-<script setup></script>
+<script setup>
+</script>
 
 <template>
-  <div class="hero-logo">
+    <div class = hero-logo>
     <img src="/homepagelogo.png" alt="STOCKFA Logo" class="logo" />
-  </div>
+    </div>
 </template>
 
 <style scoped>
-.logo {
-  width: 100%;
-  height: auto;
-  image-rendering: -webkit-optimize-contrast;
-  filter: drop-shadow(6px 6px 6px rgba(0, 0, 0, 0.5));
-  margin-top: 20px;
-  max-width: 100%;
+  .logo {
+        width: 100%;      
+        height: auto;          
+        image-rendering: -webkit-optimize-contrast;
+        filter: drop-shadow(6px 6px 6px rgba(0, 0, 0, 0.5));
+        margin-top: 20px;  
+        max-width: 100;
+    }
+
+  .hero-logo {
+    display: flex;
+    justify-content: center;
+   position: fixed;
+   top: -150px;
+   left: 50%;
+   transform: translateX(-50%);
+   z-index: 1000;
+   width: 600px;
+   padding: 1rem;
 }
 
-.hero-logo {
-  display: flex;
-  justify-content: center;
-  position: fixed;
-  top: -150px;
-  left: 50%;
-  transform: translateX(-50%);
-  z-index: 1000;
-  width: 600px;
-  padding: 1rem;
-}
 </style>
